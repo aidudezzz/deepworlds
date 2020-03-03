b5d8ace33f67ec240a37e028d4d5ad1922b5101d
tsampazk <tsampazk@csd.auth.gr>
2020-03-03 12:16:33 +0200

    Slight refactor and extra comments in PPO agent

--- examples/torch/cartPolePPO/controllers/supervisor_manager/agent/PPO_agent.py
+++ examples/torch/cartPolePPO/controllers/supervisor_manager/agent/PPO_agent.py
@@ -50,45 +50,28 @@ class PPOAgent:
         # Training stats
         self.buffer = []
 
-    def work(self, agentInput, type_="simple"):
+    def work(self, agentInput, type_="selectAction"):
         """
-        type_ == "simple"
-            Implementation for a simple forward pass.
         type_ == "selectAction"
             Implementation for the forward pass, that returns a selected action according to the probability
             distribution and its probability.
         type_ == "selectActionMax"
             Implementation for the forward pass, that returns the max selected action.
         """
-        agentInput = from_numpy(np.array(agentInput)).float().unsqueeze(0)
+        agentInput = from_numpy(np.array(agentInput)).float().unsqueeze(0)  # Add batch dimension with unsqueeze
+
         if self.use_cuda:
             agentInput = agentInput.cuda()
+
         with no_grad():
             action_prob = self.actor_net(agentInput)
 
-        if type_ == "simple":
-            output = [action_prob[0][i].data.tolist() for i in range(len(action_prob[0]))]
-            return output
-        elif type_ == "selectAction":
+        if type_ == "selectAction":
             c = Categorical(action_prob)
             action = c.sample()
             return action.item(), action_prob[:, action.item()].item()
         elif type_ == "selectActionMax":
             return np.argmax(action_prob).item(), 1.0
-        else:
-            print("Wrong type in agent.work(), returning input")
-            return [agentInput[0][i].tolist() for i in range(agentInput.size()[1])]
-
-    def getValue(self, state):
-        """
-        Gets the value of the current state according to the critic model.
-        :param state: agentInput
-        :return: state's value
-        """
-        state = from_numpy(state)
-        with no_grad():
-            value = self.critic_net(state)
-        return value.item()
 
     def save(self, path):
         """
@@ -120,15 +103,19 @@ class PPOAgent:
 
     def trainStep(self, batchSize=None):
         """
-        Performs a training step or update for the actor and critic models, based on transitions gathered in the
+        Performs a training step for the actor and critic models, based on transitions gathered in the
         buffer. It then resets the buffer.
+        :param batchSize: int or None, overrides agent set batch size
         :return: None
         """
+
+        # Default behaviour waits for buffer to collect at least one batch_size of transitions
         if batchSize is None:
             if len(self.buffer) < self.batch_size:
                 return
             batchSize = self.batch_size
 
+        # Extract states, actions, rewards and action probabilities from transitions in buffer
         state = tensor([t.state for t in self.buffer], dtype=torch_float)
         action = tensor([t.action for t in self.buffer], dtype=torch_long).view(-1, 1)
         reward = [t.reward for t in self.buffer]
@@ -142,11 +129,14 @@ class PPOAgent:
             Gt.insert(0, R)
         Gt = tensor(Gt, dtype=torch_float)
 
+        # Send everything to cuda if used
         if self.use_cuda:
             state, action, old_action_log_prob = state.cuda(), action.cuda(), old_action_log_prob.cuda()
             Gt = Gt.cuda()
 
+        # Repeat the update procedure for ppo_update_iters
         for i in range(self.ppo_update_iters):
+            # Create randomly ordered batches of size batchSize from buffer
             for index in BatchSampler(SubsetRandomSampler(range(len(self.buffer))), batchSize, False):
                 # Calculate the advantage at each step
                 Gt_index = Gt[index].view(-1, 1)
@@ -154,20 +144,21 @@ class PPOAgent:
                 delta = Gt_index - V
                 advantage = delta.detach()
 
-                # Get the current prob
+                # Get the current probabilities
+                # Apply past actions with .gather()
                 action_prob = self.actor_net(state[index]).gather(1, action[index])  # new policy
 
                 # PPO
-                ratio = (action_prob / old_action_log_prob[index])
+                ratio = (action_prob / old_action_log_prob[index])  # Ratio between current and old policy probabilities
                 surr1 = ratio * advantage
                 surr2 = clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advantage
 
                 # update actor network
                 action_loss = -torch_min(surr1, surr2).mean()  # MAX->MIN descent
-                self.actor_optimizer.zero_grad()
-                action_loss.backward()
-                nn.utils.clip_grad_norm_(self.actor_net.parameters(), self.max_grad_norm)
-                self.actor_optimizer.step()
+                self.actor_optimizer.zero_grad()  # Delete old gradients
+                action_loss.backward()  # Perform backward step to compute new gradients
+                nn.utils.clip_grad_norm_(self.actor_net.parameters(), self.max_grad_norm)  # Clip gradients
+                self.actor_optimizer.step()  # Perform training step based on gradients
 
                 # update critic network
                 value_loss = F.mse_loss(Gt_index, V)
@@ -176,6 +167,7 @@ class PPOAgent:
                 nn.utils.clip_grad_norm_(self.critic_net.parameters(), self.max_grad_norm)
                 self.critic_net_optimizer.step()
 
+        # After each training step, the buffer is cleared
         del self.buffer[:]
 
 
@@ -205,65 +197,3 @@ class Critic(nn.Module):
         x = F.relu(self.fc2(x))
         value = self.state_value(x)
         return value
-
-# Example uses of the PPOAgent
-# def train_model(env, agent, episodes=200, render=False, max_steps=200):
-#     outer_range = tqdm.tqdm(total=episodes, desc='Progress: ', position=0)
-#
-#     reward_log = tqdm.tqdm(total=0, position=1, bar_format='{desc}')
-#
-#     for _ in range(episodes):
-#
-#         state = env.reset()
-#
-#         if render:
-#             env.render()
-#
-#         total_reward = 0
-#
-#         for _ in range(max_steps):
-#             action, action_prob = agent.select_action(state)
-#             next_state, reward, done, _ = env.step(action)
-#             total_reward += reward
-#             trans = Transition(state, action, action_prob, reward, next_state)
-#             if render: env.render()
-#             agent.store_transition(trans)
-#             state = next_state
-#
-#             if done:
-#                 break
-#
-#         if len(agent.buffer) >= agent.batch_size:
-#             agent.update()
-#
-#         reward_log.set_description_str("Episode reward: %4.2f" % total_reward)
-#         outer_range.update(1)
-#
-#
-# def test_model(env, agent, episodes=200, render=False, max_steps=200, use_max=True):
-#     for i in range(episodes):
-#
-#         state = env.reset()
-#
-#         if render:
-#             env.render()
-#
-#         total_reward = 0
-#
-#         for _ in range(max_steps):
-#             if use_max:
-#                 action = agent.select_max_action(state)
-#             else:
-#                 action, action_prob = agent.select_action(state)
-#             action = action.item()
-#
-#             next_state, reward, done, _ = env.step(action)
-#             total_reward += reward
-#             if render:
-#                 env.render()
-#             state = next_state
-#
-#             if done:
-#                 break
-#
-#         print("Episode %d completed, reward = %4.3f " % (i, total_reward))
