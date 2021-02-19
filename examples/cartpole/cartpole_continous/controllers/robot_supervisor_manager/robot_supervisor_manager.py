ba3c2e849cb63d15c25c6ceb8ab63fb5d4440bfe
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2021-02-19 11:58:30 +0200

    Added stable baselines 3 PPO in comments

--- examples/cartpole/cartpole_continous/controllers/robot_supervisor_manager/robot_supervisor_manager.py
+++ examples/cartpole/cartpole_continous/controllers/robot_supervisor_manager/robot_supervisor_manager.py
@@ -4,3 +4,25 @@ More runners for continuous RL algorithms can be added here.
 import DDPG_runner
 
 DDPG_runner.run()
+
+# from robot_supervisor import CartPoleRobotSupervisor
+# from stable_baselines3.ppo import PPO
+# from stable_baselines3.common.monitor import Monitor
+# from stable_baselines3.common.callbacks import CheckpointCallback
+#
+#
+# env = Monitor(CartPoleRobotSupervisor(), filename="")
+# checkpoint_callback = CheckpointCallback(
+#     save_freq=int(1e4),
+#     save_path='./checkpoints/',
+#     name_prefix='rl_model'
+# )
+#
+# # Train
+# model = PPO(
+#     'MlpPolicy',
+#     env,
+#     tensorboard_log='./tb_logs/',
+#     verbose=1
+# )
+# model.learn(total_timesteps=int(1e6), callback=checkpoint_callback)
