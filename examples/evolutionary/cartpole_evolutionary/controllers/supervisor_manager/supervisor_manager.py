70e27591eafe1b049041cfa8a96ff9cdb1344a24
veds12 <vedantshah2012@gmail.com>
2021-08-04 15:40:05 +0530

    try different params for pygad.GA

--- examples/evolutionary/cartpole_evolutionary/controllers/supervisor_manager/supervisor_manager.py
+++ examples/evolutionary/cartpole_evolutionary/controllers/supervisor_manager/supervisor_manager.py
@@ -1,5 +1,6 @@
 from supervisor_controller import CartpoleSupervisor
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 observation_space = 4
 action_space = 2
@@ -12,5 +13,65 @@ model = nn.Sequential(
     nn.Linear(16, action_space),
 )
 
-supervisor = CartpoleSupervisor(model=model)
-supervisor.train()
+'''supervisor1 = CartpoleSupervisor(model=model)
+_, _, _, fitness1 = supervisor1.train(
+                                    num_generations=75, 
+                                    num_parents_mating=5,
+                                    num_solutions=10,
+                                    parent_selection_type="sss", 
+                                    crossover_type="single_point",
+                                    mutation_type="random",
+                                    mutation_percent_genes=10, 
+                                    keep_parents=-1,
+                                    )
+
+supervisor2 = CartpoleSupervisor(model=model)
+_, _, _, fitness2 = supervisor2.train(
+                                    num_generations=75, 
+                                    num_parents_mating=8,
+                                    num_solutions=10,
+                                    parent_selection_type="sss", 
+                                    crossover_type="single_point",
+                                    mutation_type="random",
+                                    mutation_percent_genes=10, 
+                                    keep_parents=-1,
+                                    )
+
+supervisor3 = CartpoleSupervisor(model=model)
+_, _, _, fitness3 = supervisor3.train(
+                                    num_generations=75, 
+                                    num_parents_mating=5,
+                                    num_solutions=20,
+                                    parent_selection_type="sss", 
+                                    crossover_type="single_point",
+                                    mutation_type="random",
+                                    mutation_percent_genes=10, 
+                                    keep_parents=-1,
+                                    )'''
+
+supervisor4 = CartpoleSupervisor(model=model)
+_, _, _, fitness4 = supervisor4.train(
+                                    num_generations=75, 
+                                    num_parents_mating=5,
+                                    num_solutions=5,
+                                    parent_selection_type="sss", 
+                                    crossover_type="single_point",
+                                    mutation_type="random",
+                                    mutation_percent_genes=10, 
+                                    keep_parents=-1,
+                                    )
+
+'''supervisor5 = CartpoleSupervisor(model=model)
+_, _, _, fitness5 = supervisor5.train(
+                                    num_generations=75, 
+                                    num_parents_mating=5,
+                                    num_solutions=10,
+                                    parent_selection_type="sus", 
+                                    crossover_type="single_point",
+                                    mutation_type="random",
+                                    mutation_percent_genes=10, 
+                                    keep_parents=-1,
+                                    )'''
+
+print(fitness4)
+
