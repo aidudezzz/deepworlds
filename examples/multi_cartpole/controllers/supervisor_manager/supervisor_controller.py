70748f2b688a186d6db642c4f718910ff0a35855
veds12 <vedantshah2012@gmail.com>
2021-08-17 11:01:44 +0530

    fixed multicartpole

--- examples/multi_cartpole/controllers/supervisor_manager/supervisor_controller.py
+++ examples/multi_cartpole/controllers/supervisor_manager/supervisor_controller.py
@@ -44,27 +44,28 @@ class CartPoleSupervisor(SupervisorEnv):
         Cart Position is more than absolute 0.89 on z axis (cart has reached arena edge)
         Episode length is greater than 200
         Solved Requirements (average episode score in last 100 episodes > 195.0)
-    TODO:
-    - Improve docstrings
-    - Make it agnostic to number of robots
     """
 
-    def __init__(self):
+    def __init__(self, num_robots=9):
         """
         In the constructor, the agent object is created.
         References to robots and the pole endpoints are initialized here, used for building the observation.
         When in test mode (self.test = True) the agent stops being trained and picks actions in a non-stochastic way.
+        
+        :param num_robots: Number of robots in the environment
+        :type num_robots: int
         """
         super().__init__()
 
+        self.num_robots = num_robots
         self.timestep = int(self.getBasicTimeStep())
-        self.communication = self.initialize_comms(9)
+        self.communication = self.initialize_comms()
 
         self.observationSpace = 4
         self.actionSpace = 2
-        self.robot = [self.getFromDef("ROBOT" + str(i)) for i in range(9)]
-        self.initPositions = [self.robot[i].getField("translation").getSFVec3f() for i in range(9)]
-        self.poleEndpoint = [self.getFromDef("POLE_ENDPOINT_" + str(i)) for i in range(9)]
+        self.robot = [self.getFromDef("ROBOT" + str(i)) for i in range(self.num_robots)]
+        self.initPositions = [self.robot[i].getField("translation").getSFVec3f() for i in range(self.num_robots)]
+        self.poleEndpoint = [self.getFromDef("POLE_ENDPOINT_" + str(i)) for i in range(self.num_robots)]
 
         self.messageReceived = None                     # Variable to save the messages received from the robots
 
@@ -73,9 +74,15 @@ class CartPoleSupervisor(SupervisorEnv):
         self.episodeScoreList = []                      # A list to save all the episode scores, used to check if task is solved
         self.test = False                               # Whether the agent is in test mode
 
-    def initialize_comms(self, robots_num):
+    def initialize_comms(self):
+        """
+        Initializes emitter and receiver channels for each robot
+        :return communication: A list of dictionaries, each corresponding to 1 robot with keys "emitter" and "receiver" and values
+                                as emitter and receiver instances of the robot
+        :rtype: list(dict)
+        """
         communication = []
-        for i in range(robots_num):
+        for i in range(self.num_robots):
             emitter = self.getDevice(f'emitter{i}')
             receiver = self.getDevice(f'receiver{i}')
 
@@ -92,6 +99,13 @@ class CartPoleSupervisor(SupervisorEnv):
         return communication
 
     def step(self, action):
+        """
+        Take one step in the environment - transmits the actions to be taken to the robots
+        :param action: actions to be taken by the robots in the environment
+        :type action: list(int)
+        :return: observations, rewards, done, info after taking the step
+        :rtype: tuple(list(float), list(float), bool, dict)
+        """
         if super(Supervisor, self).step(self.timestep) == -1:
             exit()
 
@@ -105,11 +119,19 @@ class CartPoleSupervisor(SupervisorEnv):
         )
 
     def handle_emitter(self, actions):
+        """
+        Emits actions to the robots through robot specific emitter channels
+        """
         for i, action in enumerate(actions):
             message = str(action).encode("utf-8")
             self.communication[i]['emitter'].send(message)
 
     def handle_receiver(self):
+        """
+        Receives actions sent by the robots in the environment through robot specific receiver channels
+        :return messages: list of messages sent by the robots
+        :rtype: list(int)
+        """
         messages = []
         for com in self.communication:
             receiver = com['receiver']
@@ -128,37 +150,37 @@ class CartPoleSupervisor(SupervisorEnv):
         The pole angle value is taken from the messages sent by the robots.
         All values are normalized appropriately to [-1, 1], according to their original ranges.
 
-        :return: Observation: [cartPosition0, cartVelocity0, poleAngle0, poleTipVelocity0],
+        :return: Observation:[[cartPosition0, cartVelocity0, poleAngle0, poleTipVelocity0],
                               [cartPosition1, cartVelocity1, poleAngle1, poleTipVelocity1], ...
-                              [cartPosition9, cartVelocity9, poleAngle9, poleTipVelocity9]
-        :rtype: tuple(list, list)
+                              [cartPosition9, cartVelocity9, poleAngle9, poleTipVelocity9]]
+        :rtype: list(list(list(float), list(float), float, float))   
         """
         # Position on z axis
-        cartPosition = [normalizeToRange(self.robot[i].getPosition()[2], -0.4, 0.4, -1.0, 1.0) for i in range(9)]
+        cartPosition = [normalizeToRange(self.robot[i].getPosition()[2], -0.4, 0.4, -1.0, 1.0) for i in range(self.num_robots)]
 
         # Linear velocity on z axis
-        cartVelocity = [normalizeToRange(self.robot[i].getVelocity()[2], -0.2, 0.2, -1.0, 1.0, clip=True) for i in range(9)]
+        cartVelocity = [normalizeToRange(self.robot[i].getVelocity()[2], -0.2, 0.2, -1.0, 1.0, clip=True) for i in range(self.num_robots)]
 
         self.messageReceived = self.handle_receiver()
 
-        poleAngle = [None for _ in range(9)]
+        poleAngle = [None for _ in range(self.num_robots)]
 
         for i, message in enumerate(self.messageReceived):    
             if message is not None:
                 poleAngle[i] = normalizeToRange(message, -0.23, 0.23, -1.0, 1.0, clip=True)
             else:
                 # method is called before messageReceived is initialized
-                poleAngle = [0.0 for _ in range(9)]
+                poleAngle = [0.0 for _ in range(self.num_robots)]
 
         # Angular velocity x of endpoint
-        endpointVelocity = [normalizeToRange(self.poleEndpoint[i].getVelocity()[3], -1.5, 1.5, -1.0, 1.0, clip=True) for i in range(9)]
+        endpointVelocity = [normalizeToRange(self.poleEndpoint[i].getVelocity()[3], -1.5, 1.5, -1.0, 1.0, clip=True) for i in range(self.num_robots)]
 
 
-        messages = [None for _ in range(9)]
-        for i in range(9):
-            messages[i] = [cartPosition[i], cartVelocity[i], poleAngle[i], endpointVelocity[i]]
+        observations = [None for _ in range(self.num_robots)]
+        for i in range(self.num_robots):
+            observations[i] = [cartPosition[i], cartVelocity[i], poleAngle[i], endpointVelocity[i]]
         
-        return messages
+        return observations
 
     def get_reward(self, action=None):
         """
@@ -184,19 +206,17 @@ class CartPoleSupervisor(SupervisorEnv):
             return True
 
         if None not in self.messageReceived:                                    # Identify which part of the message list comes from which robot
-            poleAngle = [None for _ in range(9)]
-            for message in self.messageReceived:
-                print(message)
-                robot_no = int(message[0][5])
-                poleAngle[robot_no] = round(float(message[1]), 2)
+            poleAngle = []
+            for i, message in enumerate(self.messageReceived):
+                poleAngle.append(round(float(message), 2))
 
         else:                                                                   # if method is called before messageReceived is initialized
-            poleAngle = [0.0 for _ in range(9)]
+            poleAngle = [0.0 for _ in range(self.num_robots)]
         
         if not all(abs(x) < 0.261799388 for x in poleAngle):                    # 15 degrees off vertical
             return True
 
-        cartPosition = [round(self.robot[i].getPosition()[2] - self.initPositions[i][2], 2) for i in range(9)]      
+        cartPosition = [round(self.robot[i].getPosition()[2] - self.initPositions[i][2], 2) for i in range(self.num_robots)]      
         if not all(abs(x) < 0.89 for x in cartPosition):
             return True
 
@@ -211,7 +231,7 @@ class CartPoleSupervisor(SupervisorEnv):
         """
         observation = []
 
-        for _ in range(9):
+        for _ in range(self.num_robots):
             robot_obs = [0.0 for _ in range(self.observationSpace)]
             observation.append(robot_obs)
 
@@ -241,15 +261,13 @@ class CartPoleSupervisor(SupervisorEnv):
 
     def reset(self):
         """
-        Used to reset the world to an initial state.
-        Default, problem-agnostic, implementation of reset method,
+        Used to reset the world to an initial state. Default, problem-agnostic, implementation of reset method,
         using Webots-provided methods.
-        *Note that this works properly only with Webots versions >R2020b
-        and must be overridden with a custom reset method when using
-        earlier versions. It is backwards compatible due to the fact
-        that the new reset method gets overridden by whatever the user
-        has previously implemented, so an old supervisor can be migrated
-        easily to use this class.
+        
+        *Note that this works properly only with Webots versions >R2020b and must be overridden with a custom reset method when using
+        earlier versions. It is backwards compatible due to the fact that the new reset method gets overridden by whatever the user
+        has previously implemented, so an old supervisor can be migrated easily to use this class.
+
         :return: default observation provided by get_default_observation()
         """
         self.simulationReset()
@@ -257,7 +275,7 @@ class CartPoleSupervisor(SupervisorEnv):
         super(Supervisor, self).step(int(self.getBasicTimeStep()))
         super(Supervisor, self).step(int(self.getBasicTimeStep()))
 
-        for i in range(9):
+        for i in range(self.num_robots):
             self.communication[i]['receiver'].disable()
             self.communication[i]['receiver'].enable(self.timestep)
 
